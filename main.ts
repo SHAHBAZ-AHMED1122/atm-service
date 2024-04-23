09f514074481bd6f81d4cfa2e5536202b345f7da
SHAHBAZ-AHMED1122 <161025292+SHAHBAZ-AHMED1122@users.noreply.github.com>
2024-04-23 22:32:13 +0500

    Initial commit

---
+++ main.ts
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+
+let myBalance = 10000;
+let myPin = 12345;
+const answer = await inquirer.prompt(
+  {
+    name: "pin",
+    message: " please enter your pin",
+    type: "number",
+  },
+);
+
+if (answer.pin === myPin) {
+  console.log("correct pin");
+  const operationAns = await inquirer.prompt({
+    name: "action",
+    message: "please select option",
+    type: "list",
+    choices: ["check balance", "withdraw"],
+  });
+
+  if (operationAns.action === "withdraw") {
+    let amountAns = await inquirer.prompt({
+      name: "amount",
+      message: "please enter amount",
+      type: "number",
+    });
+    myBalance -= amountAns.amount;
+    if (amountAns.amount > myBalance) {
+      console.log("you can only withdraw below your balance");
+    } else {
+      console.log("your remaining balance is", myBalance);
+    }
+  }
+  if (operationAns.action === "check balance") {
+    console.log("your balance is", myBalance);
+  }
+} else {
+  console.log("incorrect pin");
+  }
